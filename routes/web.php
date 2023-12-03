b69838fd30f51c9adc3a57ecace4487d55664a87
Dwi Setiawan <dwisetiawan099@gmail.com>
2023-12-04 01:38:01 +0700

    Task 9: /admin group within a group

--- routes/web.php
+++ routes/web.php
@@ -64,23 +64,25 @@ Route::middleware('auth')->group(function() {
     // Add a group for routes with URL prefix "admin"
     // Assign middleware called "is_admin" to them
     // Put one Route Group code line here below
-
+    Route::middleware('is_admin')->prefix('admin')->group(function() {
 
         // Tasks inside that /admin group:
-
+       
 
         // Task 10: point URL /admin/dashboard to a "Single Action" Admin/DashboardController
         // Put one code line here below
-
+        Route::get('dashboard', AdminDashboardController::class);
+        
 
         // Task 11: point URL /admin/stats to a "Single Action" Admin/StatsController
         // Put one code line here below
+        Route::get('stats', StatsController::class);
 
 
     // End of the /admin Route Group
-
+    });
 // End of the main Authenticated Route Group
-
+});
 // One more task is in routes/api.php
 
 require __DIR__.'/auth.php';
